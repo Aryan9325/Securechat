1fb30cb4365c245fc73176f1d1f05cc780fa2c40
Aryan9325 <aryansanjaysolanke@gmail.com>
2025-03-31 18:15:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
 from flask import Flask
 from flask_socketio import SocketIO
 from cryptography.fernet import Fernet
-import os
 
 # Initialize the Flask application and SocketIO
 app = Flask(__name__)
 socketio = SocketIO(app)
 
 # Your encryption key
-secret_key = "YOUR_SECRET_KEY"  # Replace with your actual secret key
+secret_key = "b'ctv1GpJD_gfZAxdFe8aKJwTi0fHDUjn-dbO6tdvQAfE="  # Replace with your actual secret key
 fernet = Fernet(secret_key)
 
 # Basic route to check if it's working
@@ -16,6 +15,6 @@ fernet = Fernet(secret_key)
 def index():
     return "SecureChat App is Running"
 
-# Starting the server with Gunicorn for production
+# Start the server with SocketIO
 if __name__ == '__main__':
     socketio.run(app, host='0.0.0.0', port=5000)
